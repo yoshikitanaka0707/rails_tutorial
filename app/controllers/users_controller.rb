ffce8a798671ce44cb3451647fc9e70380ea24fb
yoshikitanaka0707 <kishiedakinta@gmail.com>
2018-07-12 09:54:04 +0900

    ゴリラ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)   #strong parametersを使う。
     if @user.save
       # 保存の成功をここで扱う。
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
   end
 
+  private   #見えないようにできるやつ。
+
+    def user_params   #こいつらだけ書き換えて良いぞ。
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
