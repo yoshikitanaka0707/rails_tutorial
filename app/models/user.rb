dda7981b2750bcf943e08dc1433aee0cbece7397
yoshikitanaka0707 <kishiedakinta@gmail.com>
2018-07-20 21:57:23 +0900

    ゴリラ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   #バリデーションテストファイルで.valid?で呼び出せる。
   before_save { self.email = email.downcase } #データベースに渡す前にメールアドレスを全て小文字にする。データベースアダプタの中に大文字小文字を区別できないバカがいる。
   validates :name,  presence: true, length: { maximum: 50 }
@@ -23,5 +24,16 @@ class User < ApplicationRecord
   def remember
     self.remember_token = User.new_token   ###ハッシュ化した記憶トークンをremembe_token属性(インスタンス変数)に代入する。
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # (bcryptで暗号化して)渡された記憶トークンが記憶ダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)   ###記憶ダイジェストをnilにしてしまう。
   end
 end
