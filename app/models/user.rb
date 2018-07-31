ac313bc86d8f54b4c0ed2516f6caa5bbde1f0427
yoshikitanaka0707 <kishiedakinta@gmail.com>
2018-07-31 13:40:19 +0900

    ゴリラ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                foreign_key: "follower_id",
+                                dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                 foreign_key: "followed_id",
+                                 dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
   #バリデーションテストファイルで.valid?で呼び出せる。
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   #データベースに渡す前にメールアドレスを全て小文字にする。
   #データベースアダプタの中に大文字小文字を区別できないバカがいる。
@@ -47,14 +56,53 @@ class User < ApplicationRecord
     update_attribute(:activated_at, Time.zone.now)
   end
 
-  # アカウントを有効にする
-  def activate
-    update_columns(activated: true, activated_at: Time.zone.now)
-  end
+  # # アカウントを有効にする
+  # def activate
+  #   update_columns(activated: true, activated_at: Time.zone.now)
+  # end
 
   # 有効化用のメールを送信する
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest:  User.digest(reset_token), reset_sent_at: Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
+  # ユーザーのステータスフィードを返す
+  def feed
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
+  end
+
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 #StaticPagesコントローラを生成すると、(config/routes.rb)ファイルが自動的に更新される。
 #このルーティングファイルはルーターの実装を受け持ち、URLとWebページの対応関係を定義する。
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   root 'static_pages#home' #ルート「/」へのGETリクエストがStaticPagesコントローラのhomeアクションにルーティングされます。
   get 'users/new' #users/newというURLに対してgetリクエストが来たら、StaticPagesコントローラのnewアクションに渡すよう
   get  '/help',    to: 'static_pages#help'
@@ -11,6 +13,13 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'   ###ログインフォーム送信
   post   '/login',   to: 'sessions#create'   ###ログイン
   delete '/logout',  to: 'sessions#destroy'   ###ログアウト
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
